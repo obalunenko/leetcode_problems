1d6c82e2d132422e492980fcb58762b560bc65d6
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2018-04-15 23:56:58 +0300

    Solve problem 657. Judge Route Circle

--- judge_route_circle/main.go
+++ judge_route_circle/main.go
@@ -1,8 +1,71 @@
 package judge_route_circle
 
-func judgeCircle(moves string) bool {
-	var result bool
+import (
+	"fmt"
+	"strings"
+)
 
+type Robot struct {
+	position Position
+}
+
+type Position struct {
+	x int
+	y int
+}
+
+func (r *Robot) moveRight() {
+	r.position.y++
+
+}
+
+func (r *Robot) moveLeft() {
+	r.position.y--
+
+}
+
+func (r *Robot) moveUp() {
+	r.position.x++
+
+}
+
+func (r *Robot) moveDown() {
+	r.position.x--
+
+}
+
+func (r *Robot) Move(moves string) {
+
+	for _, step := range moves {
+		switch strings.ToUpper(string(step)) {
+		case "R":
+			r.moveRight()
+		case "L":
+			r.moveLeft()
+		case "U":
+			r.moveUp()
+		case "D":
+			r.moveDown()
+		default:
+			fmt.Printf("Wrong movement!")
+		}
+	}
+
+}
+
+func judgeCircle(moves string) (result bool) {
+
+	robot := Robot{
+		position: Position{
+			x: 0,
+			y: 0,
+		},
+	}
+
+	robot.Move(moves)
+	if robot.position.x == 0 && robot.position.y == 0 {
+		result = true
+	}
 	return result
 
 }
